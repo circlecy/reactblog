954423b83652f6769f123f46a53ea97245c0eb75
unknown <manileehk1@gmail.com>
2023-05-10 14:14:43 +0800

    commit

---
+++ client/src/components/posts/Posts.jsx
@@ -0,0 +1,13 @@
+import Post from "../post/Post";
+import "./posts.css";
+import "../post/post.css";
+
+export default function Posts({ posts }) {
+  return (
+    <div className="posts">
+      {posts.map((p) => (
+        <Post post={p} />
+      ))}
+    </div>
+  );
+}
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+import { useLocation } from "react-router";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
+  const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API_URL, });
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axiosInstance.get("/posts" + search);
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, [search]);
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
+}
